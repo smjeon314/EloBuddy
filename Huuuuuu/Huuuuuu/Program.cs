89d242df5b8a93730ea242f1223482472fcd94e5
smjeon314 <smjeon315@gmail.com>
2016-02-18 17:10:02 +0900

    EloBuddy
    EloBuddy Addon

---
+++ Huuuuuu/Huuuuuu/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+using Circle = EloBuddy.SDK.Rendering.Circle;
+using System.Threading;
+using EloBuddy.SDK.Menu;
+
+namespace Huuuuuu
+{
+    class Program
+    {
+        private static void DoWork()
+
+        {
+
+            while (true)
+            {
+                if (HMenu["hu"].Cast<KeyBind>().CurrentValue)
+                {
+                    if (HMenu["mode"].Cast<Slider>().CurrentValue == 1)
+                    {
+                        Player.DoEmote(Emote.Joke);
+                        Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                    }
+                    else if (HMenu["mode"].Cast<Slider>().CurrentValue == 2)
+                    {
+                        Player.DoEmote(Emote.Taunt);
+                        Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                    }
+                    else if (HMenu["mode"].Cast<Slider>().CurrentValue == 3)
+                    {
+                        Player.DoEmote(Emote.Dance);
+                        Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                    }
+                    else if (HMenu["mode"].Cast<Slider>().CurrentValue == 4)
+                    {
+                        Player.DoEmote(Emote.Laugh);
+                        Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                    }
+                }
+                Thread.Sleep(HMenu["delay"].Cast<Slider>().CurrentValue);
+            }
+        }
+
+
+        public static Menu HMenu;
+
+
+        public static void Load()
+        {
+            Mainmenu();
+
+        }
+        public static void Mainmenu()
+        {
+            HMenu = MainMenu.AddMenu("Huuuuuuu", "main");
+            HMenu.AddGroupLabel("Welcome to my firsy addon Huuuuuu");
+            HMenu.Add("hu", new KeyBind("huuuuuu active", false, KeyBind.BindTypes.HoldActive, 'U'));
+            HMenu.Add("mode", new Slider("huuuuuu mode, 1:joke, 2:taunt, 3:dance, 4:laugh", 3, 1, 4));
+            HMenu.Add("delay", new Slider("Delay", 10, 1, 1000));
+        }
+        static void Main(string[] args) { Loading.OnLoadingComplete += OnLoadingComplete; }
+
+        static void OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print("Huuuuuu by smjeon314");
+            Load();
+            Thread thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+        }
+    }
+}
---
+++ SimpleSkin/SimpleSkin/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+using Circle = EloBuddy.SDK.Rendering.Circle;
+using System.Threading;
+using EloBuddy.SDK.Menu;
+
+namespace SimpleSkin
+{
+    class Program
+    {
+        public static Menu YMenu;
+        public static void Change(Object sender, EventArgs args)
+        {
+            if (YMenu["use"].Cast<CheckBox>().CurrentValue)
+            {
+                Player.SetSkinId(YMenu["id"].Cast<Slider>().CurrentValue);
+            }
+        }
+
+        public static void Load()
+        {
+            Mainmenu();
+
+        }
+        public static void Mainmenu()
+        {
+            YMenu = MainMenu.AddMenu("SimpleSkin", "skinmain");
+            YMenu.AddGroupLabel("Welcome to my addon SimpleSkin");
+            YMenu.Add("use", new CheckBox("Use SimpleSkin"));
+            YMenu.Add("id", new Slider(Player.Instance.ChampionName + "'s SkinID", 0, 0, 15)).OnValueChange += Change;
+        }
+        static void Main(string[] args) { Loading.OnLoadingComplete += OnLoadingComplete; }
+
+        static void OnLoadingComplete(EventArgs args)
+        {
+            Chat.Print("Simple Skin by smjeon314");
+            Load();
+        }
+    }
+}
